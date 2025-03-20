5707a15b01d0d9b1a5f369444c087b7fca31b849
Melanie Roberts <robertsmelaniecc@gmail.com>
2025-03-20 16:56:58 -0400

    ymt

--- JWT-lesson-1/app.js
+++ JWT-lesson-1/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+
+const cookieParser = require ('cookie-parser');
+
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json())
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -20,4 +24,24 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 app.get('/', (req, res) => res.render('home'));
 
 app.get('/smoothies', (req, res) => res.render('smoothies'));
+
+// app.get('/set-cookies', (req, res) => {
+
+  // res.setHeader('Set-cookie',
+  //   'newUser=true');
+
+//   res.cookie('newUser', false)
+//   res.cookie('isEmployee', true, {maxAge:1000 * 60 * 24, httpOnly:true})
+//   res.send('You got the cookies, check the application tab in the inspect tool')
+  
+// })
+// app.get('/read-cookies', (req, res) => {
+  
+//   const cookies = req.cookies;
+//   console.log(cookies.newUser)
+
+//   res.json(cookies)
+// })
+
+
 app.use(authRoutes)
--- JWT-lesson-1/controllers/authController.js
+++ JWT-lesson-1/controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-
+const jwt = require('jsonwebtoken');
 const handleErrors = (err) => {
     console.log(err.message, err.code);
     let errors = { email: '', password: '' };
@@ -17,7 +17,10 @@ const handleErrors = (err) => {
     }
     return errors;
 }
-
+const maxAge = 3 * 24 * 60 *60;
+const createToken = (id) =>{
+    return jwt.sign({id}, 'secret message', {expiresIn: maxAge})
+}
 
 module.exports.signup_get = (req, res) => {
     res.render('signup')
@@ -27,10 +30,13 @@ module.exports.login_get = (req, res) => {
 }
 
 module.exports.signup_post = async (req, res) => {
-    const { email, password } = req.body;
+    const {email, password} = req.body;
     try {
         const user = await User.create({ email, password });
-        res.status(201).json(user);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000})
+
+        res.status(201).json({user: user._id})
 
     }
     catch (err) {

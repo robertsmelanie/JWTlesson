2a56afb793943f95f81713e270c2cb43a2d12693
Melanie Roberts <robertsmelaniecc@gmail.com>
2025-03-20 10:47:11 -0400

    end JWT video 5

--- JWT-lesson-1/controllers/authController.js
+++ JWT-lesson-1/controllers/authController.js
@@ -1,49 +1,54 @@
 const User = require('../models/User');
 
-const handleErrors = (err) =>{
+const handleErrors = (err) => {
     console.log(err.message, err.code);
-    let errors = {email: '', password: ''};
+    let errors = { email: '', password: '' };
 
-    if(err.code == 11000){
+    if (err.code == 11000) {
         errors.email = 'that email is already in use!';
         return errors;
     }
 
-    if(err.message.includes('user validation failed')) {
-        Object.values(err.errors).forEach(({properties})=> {
-            errors[properties.path]- properties.message
+    if (err.message.includes('user validation failed') && err.errors) {
+        Object.values(err.errors).forEach(({ properties }) => {
+            errors[properties.path] = properties.message;
         })
 
-        }
     }
+    return errors;
+}
 
 
-module.exports.signup_get = (req,res) =>{
+module.exports.signup_get = (req, res) => {
     res.render('signup')
 }
-module.exports.login_get = (req,res) =>{
+module.exports.login_get = (req, res) => {
     res.render('login')
 }
 
-module.exports.signup_post = async (req,res) =>{
-    const {email, password} = req.body;
-    try{
-        const user = await User.create({email, password});
+module.exports.signup_post = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.create({ email, password });
         res.status(201).json(user);
 
     }
-    catch(err){
+    catch (err) {
         const errors = handleErrors(err);
-        res.status(400).json({errors})
+        res.status(400).json({ errors })
 
     }
     console.log(email, password)
     // res.send('new signup')
 }
-module.exports.login_post = async (req,res) =>{
-   const {email, password} = req.body;
-    console.log(email, password)
-    console.log(req.body) 
-   res.send('user login')
-    
-}
+module.exports.login_post = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        console.log(email, password);
+        console.log(req.body);
+        res.send('user login');
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Login error');
+    }
+};
